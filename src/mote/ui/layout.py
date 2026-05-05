5c7f5c1df8e98d3a689c24d87aebd2bf501066aa
Hugo H <hugo@haaxman.co.uk>
2026-05-05 20:44:22 +0100

    Added top bar text AI Level: 0

--- src/mote/ui/layout.py
+++ src/mote/ui/layout.py
@@ -121,6 +121,10 @@ class ScreenLayout:
 
     def render(self):
         """Render all components to the screen."""
+        buffer_name = self.editor.buffer.name or "Untitled"
+        self.top.draw(0, "Mote Editor", align="left", fill_line=True)
+        self.top.draw(0, buffer_name, align="center", fill_line=False)
+        self.top.draw(0, "PROTOTYPE", align="right", fill_line=False)
         self.editor.render()
 
     def get_editor(self):
